de06a2cb360ca08696293ee9f913bf83854b6c07
EmptinessBoy <i@my.huxiaofan.com>
2021-03-05 09:44:13 +0800

    ”我的“页面继续完善，点击导航按钮可以跳转

--- pages/aboutme/aboutme.vue
+++ pages/aboutme/aboutme.vue
@@ -1,254 +1,250 @@
 <template>
-	<view class="center">
-		<view class="center_top">
-			<view class="mask">
-            </view>
-		</view>
-		<view class="center_box_bg">
-			<view class="profily">
-				<view class="base">  
-                    <u-avatar :src="avatarurl"></u-avatar>
-					<text>{{nickname}}</text>
-					<image src="../../static/fumou-center-template/setting.png" mode=""></image>
-				</view>
-				<view class="order_status">
-					<view class="status" v-for="item in status">
-						<image class="icon" :src="item.url" mode="aspectFill"></image>
-						<text>{{item.name}}</text>
-					</view>
-				</view>
-			</view>
-			<view class="baiban">
 
-			</view>
-			<view class="center_menu">
-				<view class="menu_item" v-for="item in menus">
-					<image :src="item.icon" mode="aspectFill"></image>
-					<text>{{item.name}}</text>
-				</view>
-			</view>
-		</view>
-	</view>
+    <view class="center">
+        <!-- 点击提示 -->
+        <u-modal v-model="toastwindow.show" :content="toastwindow.content"></u-modal>
+        <!-- 点击提示结束 -->
+        <view class="center_top">
+            <view class="mask">
+            </view>
+        </view>
+
+        <view class="center_box_bg">
+            <view class="profily">
+                <view class="base">
+                    <u-avatar :src="avatarurl"></u-avatar>
+                    <text>{{nickname}}</text>
+                    <!-- <image src="../../static/fumou-center-template/setting.png" mode=""></image> -->
+                </view>
+                <!-- 个人信息下方四个按钮 -->
+                <view class="order_status">
+
+                    <view class="status" v-for="item in status" @click="goPage(item.addr)">
+                        <image class="icon" :src="item.url" mode="aspectFill"></image>
+                        <text>{{item.name}}</text>
+                    </view>
+                </view>
+            </view>
+            <view class="baiban">
+
+            </view>
+            <!-- 设置项 -->
+            <view class="center_menu">
+                <view class="funcation_list">
+                    <view>
+                        <u-cell-group>
+                            <u-cell-item icon="bell" title="课表提醒" :arrow="false">
+                                <u-switch slot="right-icon" v-model="setting_remind" @change="switchRemind"></u-switch>
+                            </u-cell-item>
+                            <u-cell-item icon="edit-pen" title="教务绑定" :arrow="true"></u-cell-item>
+                            <u-cell-item icon="edit-pen" title="个人信息" :arrow="true"></u-cell-item>
+                            <u-cell-item icon="map" title="地址管理" :arrow="true"></u-cell-item>
+                        </u-cell-group>
+                    </view>
+                    <view style="margin-top: 20rpx;">
+                        <u-cell-group>
+                            <u-cell-item icon="info-circle" title="关于我们" :arrow="false" value="v0.1.2dev"></u-cell-item>
+                        </u-cell-group>
+                    </view>
+                </view>
+            </view>
+        </view>
+    </view>
 </template>
 
 <script>
     import $Storage from "../../common/storage.js"
-	export default {
-		data() {
-			return {
-                avatarurl:"../../static/fumou-center-template/header.jpg",
-                nickname:"您未登录",
-				status: [{
-						key: 1,
-						name: '待发货',
-						url: '../../static/fumou-center-template/one.png'
-					},
-					{
-						key: 2,
-						name: '待收货',
-						url: '../../static/fumou-center-template/2.png'
-					},
-					{
-						key: 3,
-						name: '待评价',
-						url: '../../static/fumou-center-template/3.png'
-					},
-					{
-						key: 4,
-						name: '全部订单',
-						url: '../../static/fumou-center-template/4.png'
-					}
-				],
-				menus: [{
-						name: '我的收藏',
-						icon: '../../static/fumou-center-template/5.png',
-						key: 1,
-					},
-					{
-						name: '地址管理',
-						icon: '../../static/fumou-center-template/6.png',
-						key: 2,
-					},
-					{
-						name: '尺码对照表',
-						icon: '../../static/fumou-center-template/7.png',
-						key: 3,
-					},
-					{
-						name: '帮助中心',
-						icon: '../../static/fumou-center-template/8.png',
-						key: 4,
-					},
-					{
-						name: '意见反馈',
-						icon: '../../static/fumou-center-template/9.png',
-						key: 5,
-					},
-					{
-						name: '关于我们',
-						icon: '../../static/fumou-center-template/10.png',
-						key: 6,
-					}
+    export default {
+        data() {
+            return {
+                avatarurl: "../../static/fumou-center-template/header.jpg",
+                nickname: "您未登录",
+                setting_remind: true,
+                toastwindow: {
+                    show: false,
+                    content: ""
+                },
+                status: [{
+                        key: 1,
+                        name: '待发货',
+                        url: '../../static/fumou-center-template/one.png',
+                        addr: '/pages/setting_jiaowu/setting_jiaowu'
+                    },
+                    {
+                        key: 2,
+                        name: '待收货',
+                        url: '../../static/fumou-center-template/2.png',
+                        addr: ''
+                    },
+                    {
+                        key: 3,
+                        name: '全部订单',
+                        url: '../../static/fumou-center-template/4.png',
+                        addr: ''
+                    },
+                    {
+                        key: 4,
+                        name: '人工客服',
+                        url: '../../static/fumou-center-template/3.png',
+                        addr: '/pages/kefu/kefu'
+                    },
+                ],
+            };
+        },
+        methods: {
+            goPage(e) {
+                console.log("准备跳转地址" + e)
+                uni.navigateTo({
+                    url: e,
+                    success: res => {},
+                    fail: () => {},
+                    complete: () => {}
+                });
+            },
+            showWindow(content) {
+                this.toastwindow.show = true,
+                    this.toastwindow.content = content
+            },
+            switchRemind(e) {
+                console.log("课表提醒状态：" + e)
+                if (e) {
+                    // 此处需要API，成功后显示window
+                    this.showWindow("课表提醒已开启")
+                } else {
+                    // 此处需要API，成功后显示window
+                    this.showWindow("课表提醒已关闭")
+                }
+            }
+        },
+        computed: {
 
-				]
-			};
-		},
-		methods: {
-
-		},
-		computed: {
-
-		},
+        },
         onShow() {
             console.log($Storage.getUserinfo())
             let userinfo = $Storage.getUserinfo()
-            
-            if(userinfo == null || userinfo.length == 0){
+
+            if (userinfo == null || userinfo.length == 0) {
                 this.nickname = "您未登录"
                 this.avatarurl = "../../static/fumou-center-template/header.jpg"
-            }else{
+            } else {
                 this.nickname = userinfo.nickName
                 this.avatarurl = userinfo.avatarUrl
                 console.log(this.avatarurl)
             }
         }
-	}
+    }
 </script>
 
 <style lang="scss">
-	page {
-		height: 100%;
-	}
+    page {
+        height: 100%;
+    }
 
-	.profily,
-	.profily_header {
-		border-radius: 8px;
-	}
+    .profily,
+    .profily_header {
+        border-radius: 8px;
+    }
 
-	.center {
-		height: 100%;
+    .center {
+        height: 100%;
 
-		&_top {
-			height: 18%;
-			background: url('../../static/fumou-center-template/header.jpg') no-repeat 0% 50%;
-			background-size: cover;
+        &_top {
+            height: 18%;
+            background: url('../../static/fumou-center-template/header.jpg') no-repeat 0% 50%;
+            background-size: cover;
 
-			// background: #E6E6E6;
-			.mask {
-				background: rgba(0, 0, 0, .4);
-				height: 100%;
-			}
-		}
+            // background: #E6E6E6;
+            .mask {
+                background: rgba(0, 0, 0, .4);
+                height: 100%;
+            }
+        }
 
-		&_box_bg {
-			background: #F9F9F9;
-			position: relative;
+        &_box_bg {
+            background: #F9F9F9;
+            position: relative;
 
-			.profily {
-				position: absolute;
-				width: 90%;
-				// border:1px solid #F7F7F7;
-				margin: 0 auto;
-				top: -100upx;
-				left: 5%;
-				background: #FEFEFE;
-				padding: 35upx;
-				box-sizing: border-box;
-				box-shadow: 0px 2px 5px #EDEDED;
-			}
-		}
-	}
+            .profily {
+                position: absolute;
+                width: 90%;
+                // border:1px solid #F7F7F7;
+                margin: 0 auto;
+                top: -100upx;
+                left: 5%;
+                background: #FEFEFE;
+                padding: 35upx;
+                box-sizing: border-box;
+                box-shadow: 0px 2px 5px #EDEDED;
+            }
+        }
+    }
 
-	.base {
-		display: flex;
-		align-items: center;
-		border-bottom: 2px solid #F6F6F6;
-		padding-bottom: 35upx;
-		position: relative;
+    .base {
+        display: flex;
+        align-items: center;
+        border-bottom: 2px solid #F6F6F6;
+        padding-bottom: 35upx;
+        position: relative;
 
-		text {
-			margin-left: 20px;
-			font-size: 30upx;
-		}
-		
-		image{
-			position: absolute;
-			height: 40upx;
-			width: 40upx;
-			right: 0px;
-			top:0px;
-		}
-	}
+        text {
+            margin-left: 20px;
+            font-size: 30upx;
+        }
 
-	.order_status {
-		display: flex;
-		justify-content: space-between;
-		margin-top: 35upx;
+        image {
+            position: absolute;
+            height: 40upx;
+            width: 40upx;
+            right: 0px;
+            top: 0px;
+        }
+    }
 
-		.status {
-			width: 140upx;
-			font-size: 24upx;
-			text-align: center;
-			letter-spacing: .5px;
-			display: flex;
-			flex-direction: column;
-			.icon {
-				width: 50upx;
-				height: 50upx;
-				margin: 0 auto;
-				margin-bottom: 5px;
-				
-			}
-		}
-	}
+    .order_status {
+        display: flex;
+        justify-content: space-between;
+        margin-top: 35upx;
 
-	.baiban {
-		background: #FEFEFE;
-		height: 300upx;
-	}
+        .status {
+            width: 140upx;
+            font-size: 24upx;
+            text-align: center;
+            letter-spacing: .5px;
+            display: flex;
+            flex-direction: column;
 
-	.center_menu {
-		width: 100%;
-		display: inline-block;
+            .icon {
+                width: 50upx;
+                height: 50upx;
+                margin: 0 auto;
+                margin-bottom: 5px;
 
-		.menu_item {
-			font-size: 28upx;
-			letter-spacing: 1px;
-			padding: 14px 5%;
-			background: #FEFEFE;
-			overflow: hidden;
-			box-sizing: border-box;
-			display: flex;
-			align-items: center;
-			position: relative;
-			border-bottom: 1px solid #EFEFEF;
+            }
+        }
+    }
 
-			&:hover {
-				background: #F6F6F6 !important;
-			}
+    .baiban {
+        background: #FEFEFE;
+        height: 300upx;
+    }
 
-			&::after {
-				content: '';
-				width: 30upx;
-				height: 30upx;
-				position: absolute;
-				right: 5%;
-				background: url('../../static/fumou-center-template/right.png') no-repeat;
-				background-size: 100%;
-			}
+    .center_menu {
+        padding: 0 35rpx 0 35rpx;
+        width: 100%;
+        display: inline-block;
+        background: #FEFEFE;
 
-			text:nth-of-type(1) {
-				margin-left: 10px;
-			}
-
-			image {
-				width: 40upx;
-				height: 40upx;
-			}
-
-			&:nth-of-type(4) {
-				margin-top: 10px;
-			}
-		}
-	}
+        .menu_item {
+            font-size: 28upx;
+            letter-spacing: 1px;
+            padding: 14px 5%;
+            background: #FEFEFE;
+            overflow: hidden;
+            box-sizing: border-box;
+            display: flex;
+            align-items: center;
+            position: relative;
+            border-bottom: 1px solid #EFEFEF;
+        }
+    }
 </style>
---
+++ pages/kefu/kefu.vue
@@ -0,0 +1,63 @@
+<template>
+    <view class="loginform">
+        <view class="innerform">
+            <image class="logo" src="../../static/logo_customer.png" mode=""></image>
+            <view style="margin-bottom: 60rpx;">
+                <text style="font-size: 30rpx; font-weight: 500;">
+                    遇到问题？外卖啥时候到？课表有问题？
+                </text>
+            </view>
+            <view style="width: 550rpx;margin-bottom: 40rpx;">
+                
+                <text style="font-size: 50rpx; font-weight: 800;">
+                    试试找客服！
+                </text>
+            </view>
+
+            <view class="button">
+                <u-button type="info" ripple="true" plain="true" class="loginbutton" :ripple="true" @getuserinfo="trydoLogin"
+                    open-type="contact">
+                    <u-icon name="kefu-ermai" style="margin-right: 20rpx;">
+                    </u-icon>联系溢予客服
+                </u-button>
+                <!-- <u-button @click="getStorage">缓存测试</u-button> -->
+            </view>
+        </view>
+
+    </view>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+
+            };
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    .loginform {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        width: 100%;
+        height: 100vh;
+
+        .button {
+            width: 550rpx;
+        }
+
+        .innerform {
+            height: 800rpx;
+        }
+
+        .logo {
+            width: 120rpx;
+            height: 120rpx;
+            margin-bottom: 40rpx;
+        }
+    }
+</style>
