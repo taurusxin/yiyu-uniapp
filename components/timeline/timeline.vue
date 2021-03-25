46308d37b25c4a9f2893b2c5e27f52f86f30925f
EmptinessBoy <i@my.huxiaofan.com>
2021-03-25 22:25:33 +0800

    新增时间轴，样式美化

---
+++ components/timeline/timeline.vue
@@ -0,0 +1,67 @@
+<template>
+	<u-time-line>
+		<u-time-line-item nodeTop="2">
+			<!-- 此处自定义了左边内容，用一个图标替代 -->
+			<template v-slot:node>
+				<view class="u-node" style="background: #3a43be;">
+					<!-- 此处为uView的icon组件 -->
+					<u-icon name="calendar-fill" color="#fff" :size="24"></u-icon>
+				</view>
+			</template>
+			<template v-slot:content>
+				<view>
+					<view class="u-order-title">活动日程</view>
+					<!-- <view class="u-order-desc">[自提柜]您的快件已放在楼下侧门，直走前方53.6米，左拐约10步，再右拐直走，见一红灯笼停下，叩门三下，喊“芝麻开门”即可。</view> -->
+					<!-- <view class="u-order-time">2019-05-08 12:12</view> -->
+				</view>
+			</template>
+		</u-time-line-item>
+		<u-time-line-item v-for="item of timeline">
+			<!-- 此处没有自定义左边的内容，会默认显示一个点 -->
+			<template v-slot:content>
+				<view>
+					<view class="u-order-desc">{{item.desc}}</view>
+					<view class="u-order-time">{{item.time}}</view>
+				</view>
+			</template>
+		</u-time-line-item>
+	</u-time-line>
+</template>
+
+<script>
+	export default{
+		props:["timeline"],
+		created() {
+			console.log(this.timeline)
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.u-node {
+		width: 44rpx;
+		height: 44rpx;
+		border-radius: 100rpx;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		background: #d0d0d0;
+	}
+	
+	.u-order-title {
+		color: #333333;
+		font-weight: bold;
+		font-size: 32rpx;
+	}
+	
+	.u-order-desc {
+		color: rgb(150, 150, 150);
+		font-size: 28rpx;
+		margin-bottom: 6rpx;
+	}
+	
+	.u-order-time {
+		color: rgb(200, 200, 200);
+		font-size: 26rpx;
+	}
+</style>
--- pages/do_activity_info/do_activity_info.vue
+++ pages/do_activity_info/do_activity_info.vue
@@ -1,8 +1,7 @@
 <template>
     <view class="content">
         <view class="image">
-            <u-image width="100%" height="400rpx" src="https://t7.baidu.com/it/u=1819248061,230866778&fm=193&f=GIF"
-                mode="aspectFill"></u-image>
+            <u-image width="100%" height="400rpx" :src="img" mode="aspectFill"></u-image>
         </view>
         <view class="mask" :style="'opacity:' + mask_opacity"></view>
         <view class="title">
@@ -11,12 +10,19 @@
             </text>
         </view>
         <view class="detail">
-            <view class="card1" style="height: 2000rpx; width: 100%; background-color: #0081FF;margin-top: 50rpx;">
+            <view class="time">
+                <u-icon name="clock-fill" style="margin-right: 20rpx;"></u-icon> {{time}}
+            </view>
+            <u-divider half-width="100%" margin-top="25" :use-slot="false"></u-divider>
+            <view class="desc">{{desc}}</view>
+            <view class="step">
+                <timeline :timeline="timeline"></timeline>
+            </view>
 
+<!--            <view class="card1" style="height: 2000rpx; width: 100%; background-color: #0081FF;margin-top: 50rpx;">
             </view>
             <view class="card1" style="height: 2000rpx; width: 100%; background-color: #95ff53;margin-top: 50rpx;">
-
-            </view>
+            </view> -->
         </view>
 
     </view>
@@ -27,8 +33,23 @@
         data() {
             return {
                 mask_opacity: 0,
+                img: "https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=3361641725,2908210208&fm=26&gp=0.jpg",
+                time: "2020 年 3 月 25 日",
                 name: "第三届吃狗粮大赛",
-
+                desc: "小王总说是加夜班，其实就是和朋友去喝酒喝到天亮，最后还是我向小王支招，这新媳妇就该弄个下马威，不能纵容，就该揍，揍到服了！小王一听有理，回去就找他媳妇理论，被他媳妇狠狠揍了一顿，过了几天，小王对我说：哥，你这方法真牛！媳妇把我治服了，媳妇还说以后不能和你这种人来往，拜拜。",
+                timeline: [{
+                        "desc": "见一见大名鼎鼎的杜嘉诚老师",
+                        "time": "与杜嘉诚老师见面"
+                    },
+                    {
+                        "desc": "学会软件设计",
+                        "time": "学会常用的UI设计软件"
+                    },
+                    {
+                        "desc": "咕咕咕",
+                        "time": "哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈"
+                    }
+                ],
             };
         },
         onPageScroll(e) {
@@ -96,18 +117,41 @@
         margin-top: 200rpx;
         width: 100%;
         height: 120rpx;
-        // background-color: #19BE6B;
         // position: -webkit-sticky;
         // position: sticky;
         // top: 0;
     }
 
     .detail {
-        padding: 30rpx;
+        padding: 35rpx;
         margin-top: 10rpx;
         width: 100%;
         min-height: 100vh;
         background-color: #FFFFFF;
         border-radius: 45rpx 45rpx 0 0;
+
+        .time {
+            font-size: 35rpx;
+            font-weight: 700;
+        }
+
+        .desc {
+            border-radius: 15rpx;
+            padding: 25rpx;
+            font-size: 27rpx;
+            line-height: 38rpx;
+            width: 100%;
+            background-color: #c9dfff;
+            margin: 40rpx 0rpx 60rpx 0rpx;
+            letter-spacing: 4rpx;
+            text-align: justify;
+            // text-align-last: justify;
+        }
+
+        .step {
+            width: 100%;
+            padding: 0 30rpx 0 30rpx;
+            margin: 0rpx 0rpx 40rpx 0rpx;
+        }
     }
 </style>
