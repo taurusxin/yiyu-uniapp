e34886eeb6c5a6832b9cfc428202d19416a242ea
EmptinessBoy <i@my.huxiaofan.com>
2021-03-31 12:25:05 +0800

    还原误删

---
+++ pages/swiper_desc/swiper_desc.vue
@@ -0,0 +1,25 @@
+<template>
+    <view>
+        <web-view :webview-styles="webviewStyles" :src="src"></web-view>
+    </view>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                src: "",
+            };
+        },
+        onLoad(e) {
+            let id = e.id
+            let src = e.src
+            console.log(id, src)
+            this.src = src
+        }
+    }
+</script>
+
+<style lang="scss">
+
+</style>
